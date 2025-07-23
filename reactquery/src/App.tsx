8769e16780eb73c1b8391c8de8eda172db9caf9f
Be-Asia <amerigirl29@gmail.com>
2025-07-23 14:56:20 -0700

    working through void title issue

--- reactquery/src/App.tsx
+++ reactquery/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 const POSTS = [
   { id: 1, title: "Post 1" },
@@ -6,11 +7,35 @@ const POSTS = [
 ];
 
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() =>
+      POSTS.push({id: 3, title }))
+    }
+  })
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
   return (
     <>
-      <h1>TanStack Query 1</h1>
+     {postsQuery.data?.map(post => (
+      <div key={post.id}>{post.title}</div>
+     ))}
+     <button onClick={()=> new }>
+      Add New
+     </button>
     </>
   );
+  function wait(duration: number) {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+  }
 }
 
 export default App;
