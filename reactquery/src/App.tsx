8632996d045ab7005f1b18fe642a65e4e6eb5967
Be-Asia <amerigirl29@gmail.com>
2025-07-23 13:56:33 -0700

    initial commit; working with tanstackquery

---
+++ reactquery/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+function App() {
+  return (
+    <>
+      <h1>TanStack Query 1</h1>
+    </>
+  );
+}
+
+export default App;
